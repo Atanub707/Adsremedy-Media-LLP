8a47963986f605511846a145cfedee93afbef8bc
Atanu Biswas <103525450+Atanub707@users.noreply.github.com>
2024-04-05 22:37:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('1 Hello World Save And Refresh the page no need to build the code every time. Its working because I am adding "nodemon" in dockerfile its refresh the page in every save its help the developer who want to test the code throught dockerfile into the local . thank you happy code .  !')
+  res.send('Hello World Save And Refresh the page no need to build the code every time. Its working because I am adding "nodemon" in dockerfile its refresh the page in every save its help the developer who want to test the code throught dockerfile into the local . thank you happy code .  !')
 })
 
 app.listen(port, () => {
